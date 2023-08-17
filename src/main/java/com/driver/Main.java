2e1ecbc944ce1fd0606d9ad9bfc92b8e4be40e51
Navneet <navneetmui@gmail.com>
2023-08-17 19:40:24 +0530

    Tasks done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //member has private access in com.driver.RWOnly
+
+        obj.setMember("New name");
+        System.out.println(obj.getMember());
+    }
 }
